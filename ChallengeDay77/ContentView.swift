1133e30a3f674e4697df4098b0c92f00a3685568
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-27 08:56:27 +0900

    Initial Commit

---
+++ ChallengeDay77/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChallengeDay77
+//
+//  Created by KazukiNakano on 2020/07/27.
+//  Copyright © 2020 dmb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
