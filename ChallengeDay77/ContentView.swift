cac4cf6f97887030a4795ba4a62d6af0f3a36043
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-27 09:32:15 +0900

    add Milestone: Projects 13-15
    https://www.hackingwithswift.com/100/swiftui/77

--- ChallengeDay77/ContentView.swift
+++ ChallengeDay77/ContentView.swift
@@ -7,10 +7,84 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var selectedImage: UIImage?
+    @State private var showingAlert = false
+    @State private var showingAlertInput = false
+    @State private var alertInput = ""
+        
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+                    
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    self.showingImagePicker = true
+                }
+                .padding(.vertical)
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
+        }
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$selectedImage)
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = selectedImage else { return }
+        image = Image(uiImage: inputImage)
+        
+        setImageName()
+    }
+    
+    func setImageName() {
+        
+        // input image name
+        saveImage()
+    }
+    
+    func saveImage() {
+        guard let processedImage = self.selectedImage
+            else {
+                self.showingAlert = true
+                return
+        }
+        
+        let imageSaver = ImageSaver()
+        
+        imageSaver.successHandler = {
+            print("Success!")
+        }
+        
+        imageSaver.errorHandler = {
+            print("Oops: \($0.localizedDescription)")
+        }
+        
+        imageSaver.writeToPhotoAlbum(image: processedImage)
+        
+        addImageAndName()
+    }
+    
+    func addImageAndName() {
+        
     }
 }
 
