288e8cd1f17e5eba8295e663b76573064c5b1b55
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-28 09:33:11 +0900

    add Time for MapKit
    https://www.hackingwithswift.com/100/swiftui/78

--- ChallengeDay77/ContentView.swift
+++ ChallengeDay77/ContentView.swift
@@ -10,81 +10,101 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
+//struct ContentView: View {
+//    @State private var image: Image?
+//    @State private var showingImagePicker = false
+//    @State private var selectedImage: UIImage?
+//    @State private var showingAlert = false
+//    @State private var showingAlertInput = false
+//    @State private var alertInput = ""
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                ZStack {
+//                    Rectangle()
+//                        .fill(Color.secondary)
+//
+//                    if image != nil {
+//                        image?
+//                            .resizable()
+//                            .scaledToFit()
+//                    } else {
+//                        Text("Tap to select a picture")
+//                            .foregroundColor(.white)
+//                            .font(.headline)
+//                    }
+//                }
+//                .onTapGesture {
+//                    self.showingImagePicker = true
+//                }
+//                .padding(.vertical)
+//            }
+//            .padding([.horizontal, .bottom])
+//            .navigationBarTitle("Instafilter")
+//        }
+//        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+//            ImagePicker(image: self.$selectedImage)
+//        }
+//    }
+//
+//    func loadImage() {
+//        guard let inputImage = selectedImage else { return }
+//        image = Image(uiImage: inputImage)
+//
+//        setImageName()
+//    }
+//
+//    func setImageName() {
+//
+//        // input image name
+//        saveImage()
+//    }
+//
+//    func saveImage() {
+//        guard let processedImage = self.selectedImage
+//            else {
+//                self.showingAlert = true
+//                return
+//        }
+//
+//        let imageSaver = ImageSaver()
+//
+//        imageSaver.successHandler = {
+//            print("Success!")
+//        }
+//
+//        imageSaver.errorHandler = {
+//            print("Oops: \($0.localizedDescription)")
+//        }
+//
+//        imageSaver.writeToPhotoAlbum(image: processedImage)
+//
+//        addImageAndName()
+//    }
+//
+//    func addImageAndName() {
+//
+//    }
+//}
+
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var selectedImage: UIImage?
-    @State private var showingAlert = false
-    @State private var showingAlertInput = false
-    @State private var alertInput = ""
-        
+    let locationFetcher = LocationFetcher()
+
     var body: some View {
-        NavigationView {
-            VStack {
-                ZStack {
-                    Rectangle()
-                        .fill(Color.secondary)
-                    
-                    if image != nil {
-                        image?
-                            .resizable()
-                            .scaledToFit()
-                    } else {
-                        Text("Tap to select a picture")
-                            .foregroundColor(.white)
-                            .font(.headline)
-                    }
-                }
-                .onTapGesture {
-                    self.showingImagePicker = true
-                }
-                .padding(.vertical)
+        VStack {
+            Button("Start Tracking Location") {
+                self.locationFetcher.start()
             }
-            .padding([.horizontal, .bottom])
-            .navigationBarTitle("Instafilter")
+
+            Button("Read Location") {
+                if let location = self.locationFetcher.lastKnownLocation {
+                    print("Your location is \(location)")
+                } else {
+                    print("Your location is unknown")
+                }
+            }
         }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$selectedImage)
-        }
-    }
-    
-    func loadImage() {
-        guard let inputImage = selectedImage else { return }
-        image = Image(uiImage: inputImage)
-        
-        setImageName()
-    }
-    
-    func setImageName() {
-        
-        // input image name
-        saveImage()
-    }
-    
-    func saveImage() {
-        guard let processedImage = self.selectedImage
-            else {
-                self.showingAlert = true
-                return
-        }
-        
-        let imageSaver = ImageSaver()
-        
-        imageSaver.successHandler = {
-            print("Success!")
-        }
-        
-        imageSaver.errorHandler = {
-            print("Oops: \($0.localizedDescription)")
-        }
-        
-        imageSaver.writeToPhotoAlbum(image: processedImage)
-        
-        addImageAndName()
-    }
-    
-    func addImageAndName() {
-        
     }
 }
 
---
+++ ChallengeDay77/CurrentLocationGetter.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentLocationGetter.swift
+//  ChallengeDay77
+//
+//  Created by KazukiNakano on 2020/07/28.
+//  Copyright © 2020 dmb. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
